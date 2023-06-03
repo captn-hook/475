4976ff8188081c6eb31a447c8f2fa58bd042b8a8
Tristan <tristanskyhook@gmail.com>
2023-06-03 15:54:05 -0700

    laptop

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ for s in 128 256 512 1024 2048 4096
 do
     for b in 1 2 4 8 16
     do
-        g++ -DMATW=$s -DLOCALSIZE=$b -o proj06 proj06.cpp -l -lm -fopenmp
+        rm proj06
+        g++ -DMATW=$s -DLOCALSIZE=$b -o proj06 proj06.cpp -lOpenCL -lm -fopenmp ./proj06
         ./proj06
     done
 done
